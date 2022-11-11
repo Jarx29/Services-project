7b0f75e562c5c82e21a49abad932758f9d01b27e
192845-AlexisO <al192845@alumnos.uacj.mx>
2022-11-10 19:26:18 -0700

    Update models.py
    Models inclompleto

--- services/Core/models.py
+++ services/Core/models.py
@@ -3,4 +3,41 @@ from django.contrib.auth.models import BaseUserManager,AbstractBaseUser, Permiss
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
 
+    def create_user(self, username, email=None, password=None, **extrafields):
+        user = self.model(username=username, email=email, **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+class UserCliente(AbstractBaseUser, PermissionsMixin):
+    nombre = models.CharField(max_length=30)
+    apellidoP = models.CharField(max_length=20)
+    apellidoM = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=20)
+    fechaDeNacimiento = models.DateField
+    password = models.CharField(max_length=20)
+    telefono = models.IntegerField(max_length=15)
+    tipoCuenta = models.BooleanField(default=False)
+    direccion = models.CharField(max_length=50)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
+class UserPrestador(AbstractBaseUser, PermissionsMixin):
+    nombre = models.CharField(max_length=30)
+    apellidoP = models.CharField(max_length=20)
+    apellidoM = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=20)
+    fechaDeNacimiento = models.DateField
+    password = models.CharField(max_length=20)
+    telefono = models.IntegerField(max_length=15)
+    tipoCuenta = models.BooleanField(default=False)
+    telefonoPrestador = models.IntegerField(max_length=15)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
